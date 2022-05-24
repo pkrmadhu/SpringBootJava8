16f79da16ab8c2125332c48b323a0cd8ed4631ea
pkrmadhu <pkr.madhu@gmail.com>
2022-05-24 10:17:05 +0530

    2nd commit -24-may-2022

--- springbootrestjavaeight/src/main/java/com/springbootrestapi/controller/EmployeeController.java
+++ springbootrestjavaeight/src/main/java/com/springbootrestapi/controller/EmployeeController.java
@@ -3,7 +3,12 @@ package com.springbootrestapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,18 +16,29 @@ import com.springbootrestapi.model.Employee;
 import com.springbootrestapi.service.EmployeeService;
 
 @RestController
-@RequestMapping("/employees")
+@RequestMapping("/api")
 public class EmployeeController {
-	
-	@Autowired
-	private EmployeeService es ;
-	
-	@GetMapping("/allEmployees")
-	//public List<Employee> getAllEmployees() {
-	public String getAllEmployees() {
-		return "All Employee Details";
-		
-		//return es.getAllEmployees();
-	}
+        @Autowired
+        EmployeeService empService;
+        
+        @PostMapping("/employee")
+        public Employee createEmployee(@RequestBody Employee emp) {
+            return empService.createEmployee(emp);
+        }
+        
+        @GetMapping("/employees")
+        public List<Employee> readEmployees() {
+            return empService.getEmployees();
+        }
+
+        @PutMapping("/employees/{empId}")
+        public Employee readEmployees(@PathVariable(value = "empId") Integer empId, @RequestBody Employee empDetails) {
+            return empService.updateEmployee(empId, empDetails);
+        }
+
+        @DeleteMapping("/employees/{empId}")
+        public void deleteEmployees(@PathVariable(value = "empId") Integer empId) {
+            empService.deleteEmployee(empId);
+        }
 
 }
--- springbootrestjavaeight/src/main/java/com/springbootrestapi/service/EmployeeService.java
+++ springbootrestjavaeight/src/main/java/com/springbootrestapi/service/EmployeeService.java
@@ -5,20 +5,42 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.springbootrestapi.dao.EmployeeDataRepository;
+import com.springbootrestapi.dao.EmployeeRepository;
 import com.springbootrestapi.model.Employee;
 
 @Service
 public class EmployeeService {
 	
 	@Autowired
-	private EmployeeDataRepository edr;
+	private EmployeeRepository empRepository;
 
-	public List<Employee> getAllEmployees() {
-		
-		return edr.findAll();
+	// CREATE 
+	public Employee createEmployee(Employee emp) {
+	    return empRepository.save(emp);
+	}
+
+	// READ
+	public List<Employee> getEmployees() {
+	    return empRepository.findAll();
+	}
+
+	// DELETE
+	public void deleteEmployee(Integer empId) {
+	    empRepository.deleteById(empId);
 	}
 	
+	// UPDATE
+	public Employee updateEmployee(Integer empId, Employee employeeDetails) {
+	        Employee emp = empRepository.findById(empId).get();
+	        emp.setName(employeeDetails.getName());
+	        emp.setSalary(employeeDetails.getSalary());
+	        emp.setJob(employeeDetails.getJob());
+	        emp.setAge(employeeDetails.getAge());
+	        emp.setGender(employeeDetails.getGender());
+	        emp.setLocation(employeeDetails.getLocation());
+	        
+	        return empRepository.save(emp);                                
+	}
 	
 
 }
